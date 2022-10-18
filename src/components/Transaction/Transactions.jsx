24edc9ccdfec97e0eacd046e2241cefee40d4849
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-10-18 20:49:29 +0300

    r
    r

--- src/components/Transaction/Transactions.jsx
+++ src/components/Transaction/Transactions.jsx
@@ -1,6 +1,6 @@
 // import Statistics from './statistics';
 import TransactionsItem from './Item/TransactionsItem';
-import { Table, TableHead, TableR, TableH, TBody } from './transactions.styled';
+import { Table, TableHead, TableR, TableH, TBody } from './Transactions.styled';
 
 export const Transactions = ({ transData }) => {
   return (
